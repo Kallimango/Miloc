c67c79f432d16d4a7ef3c072c483a309f8de7972
Kalli <kalli@fedora.fritz.box>
2025-10-17 15:10:19 +0200

    new update

--- miloc/urls.py
+++ miloc/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("api.urls"))
+    path("api/", include("api.urls")),
+    path("", include("core.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
